2cee5c4972f0bb9965c093dfaa2fbf26eaeb9b88
Angel078m <ang89m@gmail.com>
2024-12-07 12:59:58 -0500

    Added Docstrings and comments for contexts purposes

--- app/graph.py
+++ app/graph.py
@@ -5,6 +5,7 @@ import altair as alt
 
 def chart(df, x: str, y: str, target: str) -> Chart:
 
+    """Calling the DataBase Class to retrieve the Dataframe method"""
     db = Database()
     df = db.dataframe()
 
@@ -13,12 +14,15 @@ def chart(df, x: str, y: str, target: str) -> Chart:
         print("Connection could not be established")
         return None
 
+    #Setting the title for the Chart
     chart_title= f'{y} and {x} for {target}'
 
+    #For debugging purposes if case the column does not exist
     missing_columns = [col for col in [x, y, target] if col not in df.columns]
     if missing_columns:
         raise ValueError(f"the following are missing in the DataFrame")
 
+    """Create the chart using the altair library"""
     graph = alt.Chart(df).mark_circle(size=100, stroke='black', strokeWidth=2).encode(
       x= alt.X(x, title=x),
       y= alt.Y(y, title=y),
@@ -30,7 +34,7 @@ def chart(df, x: str, y: str, target: str) -> Chart:
         height=600
     )
 
-
+    #Changing the style of the chart to match the Bandersnatch Website's Dark Theme
     graph = graph.configure_view(
         stroke=None
     ).configure(background='black'
